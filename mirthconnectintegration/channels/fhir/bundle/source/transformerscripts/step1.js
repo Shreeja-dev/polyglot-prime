6cd49c06902b572997ff9d285472f3fc285f0625
Shreeja jobin <shreeja-jobin@netspective.in>
2025-03-18 12:52:59 +0530

    feat: update FHIR bundle configuration and enhance request URI handling

--- mirthconnectintegration/channels/fhir/bundle/source/transformerscripts/step1.js
+++ mirthconnectintegration/channels/fhir/bundle/source/transformerscripts/step1.js
@@ -2,6 +2,7 @@
 var processFHIRBundle = globalMap.get("processFHIRBundle");
 if (processFHIRBundle) {
     var tenantId = $('headers').getHeader('X-TechBD-Tenant-ID');
+    channelMap.put("requestUri","/Bundle");
     var validationResults = processFHIRBundle(tenantId, channelMap, connectorMessage, responseMap);
     responseMap.put("resultJSON",validationResults);
 } else {
