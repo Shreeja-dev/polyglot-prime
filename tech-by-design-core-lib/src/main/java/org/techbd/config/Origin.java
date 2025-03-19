ed8175d152462689d5c2748a8616b43a97506e9a
Shreeja jobin <shreeja-jobin@netspective.in>
2025-03-19 10:29:03 +0530

    chore: remove obsolete compiled classes and jar files from the project

--- tech-by-design-core-lib/src/main/java/org/techbd/config/Origin.java
+++ tech-by-design-core-lib/src/main/java/org/techbd/config/Origin.java
@@ -1,7 +1,15 @@
 package org.techbd.config;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public enum Origin {
     HTTP,
-    SFTP
+    SFTP;
 
+    public static String getOriginsAsString() {
+        return Arrays.stream(Origin.values())
+                .map(Enum::name)
+                .collect(Collectors.joining(", "));
+    }
 }
